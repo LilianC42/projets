41a850e7face952ebd0c31f51e9b12ceb1937aae
LilianC42 <146996621+LilianC42@users.noreply.github.com>
2023-12-10 18:31:36 +0100

    Add files via upload

---
+++ CV/script.js
@@ -0,0 +1,9 @@
+function NouvelleFenetre1() {
+  var url = 'https://github.com/LilianC42/projets';
+  window.open(url, '_blank');
+}
+
+function NouvelleFenetre2() {
+  var url = 'https://www.linkedin.com/in/lilian-castella-51b02828a';
+  window.open(url, '_blank');
+}
