e03aa34ef60963ecef6c18d85c7e62b34d9be9b4
LilianC <100050930+Lame34@users.noreply.github.com>
2024-01-31 16:22:31 +0100

    Delete fr directory

--- fr/pages/scripts/home.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simulez une pause de 3 secondes (3000 millisecondes)
-    setTimeout(function() {
-        document.getElementById("loader").classList.add("loaded");
-        document.body.style.overflow = "visible"; // Permet de réactiver le défilement
-    }, 3000);
-});
